20286522fb55545488c29d41bdcb974649d1946c
Ryder Cook <48497255+RC00K@users.noreply.github.com>
2022-03-24 13:28:06 -0500

    Update db_connect.php

--- model/db_connect.php
+++ model/db_connect.php
@@ -1,10 +1,10 @@
 <?php
-$dsn = 'mysql:host=acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=xyfwum8m7ktq6odw';
-$username = 'qlad65c9vhnj24av';
-$password = 'l3ckqwlzko0djj07';
+$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
+$username = 'root';
+//$password = '1qaz2WSX';
 
 try {
-    $db = new PDO($dsn, $username, $password);
+    $db = new PDO($dsn, $username);
 } catch (PDOException $e) {
     $error = "Database Error: ";
     $error .= $e->getMessage();
