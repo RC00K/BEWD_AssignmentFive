6a96d616d0c667cc2ec7b9343398de348d9a2872
Ryder Cook <48497255+RC00K@users.noreply.github.com>
2022-04-06 15:54:48 -0500

    Update db_connect.php

--- model/db_connect.php
+++ model/db_connect.php
@@ -1,10 +1,10 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-$username = 'root';
-//$password = '1qaz2WSX';
+$dsn = 'mysql:host=acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=xyfwum8m7ktq6odw';
+$username = 'qlad65c9vhnj24av';
+$password = 'l3ckqwlzko0djj07';
 
 try {
-    $db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 } catch (PDOException $e) {
     $error = "Database Error: ";
     $error .= $e->getMessage();
